e9c6e1ce14e40b292ed0faaed160758a896584f5
SUMIT DHAMA <144706400+Sumit19dhama@users.noreply.github.com>
2025-06-23 20:08:06 +0530

    Add files via upload

---
+++ Javascript/script1.js
@@ -0,0 +1,7 @@
+// ================================== typing animaton ========================== //
+var typed = new Typed(".typing",{
+    strings:["Frontened Developer","Backened Developer","Java Developer", "Bloger"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
